e70430176bab0c3cf050d17a668248e76fe60a24
745700 <17614134344@163.com>
2026-06-19 21:20:20 +0800

    proper ebooklib stub

--- aion-chat/vendor/ebooklib.py
+++ aion-chat/vendor/ebooklib.py
@@ -1,1 +1,14 @@
 # ebooklib stub for Render
+class Book:
+    def __init__(self, *a, **k): pass
+    def get_item(self, *a, **k): return None
+    def get_items(self, *a, **k): return []
+    def process_content(self, *a, **k): return []
+class Epub:
+    def __init__(self, *a, **k): self.book = Book()
+    def process_content(self, *a, **k): pass
+    def add_author(self, *a, **k): pass
+    def add_item(self, *a, **k): pass
+    def write(self, *a, **k): pass
+epub = type("EpubModule", (), {"Book": Book, "Epub": Epub})()
+
